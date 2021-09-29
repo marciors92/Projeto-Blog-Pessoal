a285ad3ab545d48bf5724ac212c1b9cbd0182487
unknown <marciors92@gmail.com>
2021-09-28 21:31:32 -0300

    CRUD atualizado

--- blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagem.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table (name = "tb_postagens")
@@ -29,6 +32,10 @@ public class Postagem {
 	
 	@Temporal (TemporalType.TIMESTAMP)
 	private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties ("postagem")
+	private Tema tema;
 	
 	
 	public long getId() {
@@ -49,11 +56,17 @@ public class Postagem {
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
-	public Date getDate() {
+	public Date getData() {
 		return data;
 	}
-	public void setDate(Date data) {
+	public void setData(Date data) {
 		this.data = data;
+	}
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 	
 	
